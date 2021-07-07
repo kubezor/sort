6ab670393ba847b4fe621494032e843b6d6d25f1
7 <59332070+kubezor@users.noreply.github.com>
2021-07-07 10:00:29 +0400

    Add files via upload

---
+++ sort/src/js/dist/script copy.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function () {
+  var panelItem = document.querySelectorAll('.panel-title'),
+      bodyItem = document.querySelectorAll('.panel-body');
+  panelItem.__proto__.forEach = [].__proto__.forEach;
+  var activePanel;
+  panelItem.forEach(function (item, i, panelItem) {
+    item.addEventListener('click', function (e) {
+      //show new thingy;
+      this.classList.add('panel-active');
+      this.nextElementSibling.classList.add('active'); //hide old thingy
+
+      if (activePanel) {
+        activePanel.classList.remove('panel-active');
+        activePanel.nextElementSibling.classList.remove('active');
+      } //update thingy
+
+
+      activePanel = activePanel === this ? 0 : this;
+    });
+  });
+});
---
+++ sort/src/js/dist/script_1.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.getElementsByClassName('bg_lm1')[0].addEventListener('click', function (event) {
+  // do something
+  console.log('123');
+});
